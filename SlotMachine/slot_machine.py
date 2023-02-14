48ec331e8048586a4114ace8a4dd4afb1c4253af
2309 <raoashish2309@gmail.com>
2023-02-14 14:04:07 +0530

    First release of Hello World

---
+++ SlotMachine/slot_machine.py
@@ -0,0 +1,13 @@
+"""Project to simulate slot machines"""
+
+def deposit():
+  """User's deposit"""
+  while True :
+    amount = input("What would you like to deposit? $")
+    if amount.isdigit():
+      amount = int(amount)
+      if amount>0: break
+      else: print("Amount must be greater than 0.")
+    else : print("Please enter a number.")
+
+amount = deposit()
